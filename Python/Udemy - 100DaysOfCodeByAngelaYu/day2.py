b54960648721cb01e18f71bb679056b03562e8a9
Michael Koueviakoe <MichaelK2864@outlook.com>
2026-09-17 13:35:28 -0500

    Updated day 2 files

--- Python/Udemy - 100DaysOfCodeByAngelaYu/day2.py
+++ Python/Udemy - 100DaysOfCodeByAngelaYu/day2.py
@@ -1,102 +1,69 @@
 # Subscripting
-print("Hello"[4])
-
+print("Hello"[0])
 print("Hello"[-1])
 
 # String
+print("123" + "456") # concantination
 
-print("123" + "345") # concantanation 
-
-# Integer = whole number
-
-print(123 + 345)
-
-# Large Integers
-
+#Integer = Whole numbers
+print(123 + 456)
 print(123_456_789)
 
-print(123456789)
-
-# Float = floating point number
-
+# Float = Floating Point Number 
 print(3.14159)
 
 # Boolean
 print(True)
-print(False) 
+print(False)
 
-# Len() only works with things that are sized such as Lists and strings
+# Checking error & Type
+len("Hello") # functions take a certain type of data
 
-# Type() 
-print(type("Hello"))
 print(type(123))
 print(type(3.14))
 print(type(True))
+print(type("Hello"))
 
-# Type casting/ Type conversion 
-
+# type casting/conversion
+int("123")
 print(int("123") + int("456"))
 
-# Value errors occur from trying to convert type of something not logical. Example: cant convert abc into an integer
+print("Number of letters in your name: " + str(len(input("Enter your name "))))
 
-int()
-float()
-str()
-bool()
+# Mathematical operators
+print(123 + 456) # addition
+print(7 - 3) # subtraction
+print(3 * 2) # multiplication
+print(6 / 3) # division (implicit type casting into float)
+print(6 // 3) # removes numbers after decimal point
+print ( 2 ** 2) # exponent
 
-print("Number of letters in your name: " + str(len(input("What is your name? "))))
+# PEMDAS (order of operations)
 
-name_of_the_user = input("Enter your name ")
-length_of_name = len(name_of_the_user)
-
-print(type("Number of letters in your name: ")) # str
-print(type(length_of_name)) # int
-
-# Mathematical Operations
-
-print("My age: " + str(12))
-print(123 + 456)
-print(7 - 3) 
-print(3 * 2)
-print(6 / 3)
-# for division to result in an integer instead of float, use //
-# be careful because the double slash does not round, it just removes anything after the decimal point
-print(6//3)
-print(2**2) # exponental, so this is equivalent to 2^2 = 4
-
-# Python uses PEMDAS method too
+# Flooring, rounding, choosing digit place to cut off
 
 bmi = 84 / 1.65 ** 2
-
 print(bmi)
 
 print(int(bmi))
 
-print(round(bmi))
+print(round(bmi, 2))
 
-print(round(bmi, 3))
+#Number manipulation
 
-score = 0 
-
-# User scores a point
-
+score = 0
 score += 1
 print(score)
 
-# f-strings
-
+# f - strings
 score = 0
-height = 1.8
+height = 1.67
 is_winning = True
+print(f"Your score is {score}, your height is {height}. You are winning is {is_winning}" )
 
-print(f"Your score is = {score}, your height is {height}, and you are winning is {is_winning}")
-
-
-# Day 2 project
-print("Welcome to tip calculator!")
-bill = float(input("What is the total bill? $")) 
-tip = 1 + round((float(input("How much tip would you like? ")) / 100), 2)
-split = int(input("How many people are splitting the bill? "))
-# Calculated final bill for everyone
-Total = round(((bill * tip)/ split), 2)
-print(f"Each person should pay: ${Total}" )
+bill = float(input("Welcome to the tip calculator!\n What was the total bill? $"))
+tip = float(input("How much tip would you like to give? 10, 12, or 15? "))
+split = float(input("How many people to split the bill? "))
+percentage = float(1 + (tip / 100))
+final = round(((bill * percentage) / split), 2)
+print(f"Each person should pay: ${final}")
